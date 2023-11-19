74433140beb289a5b05d94fcacea5eec684b83c2
Saiful264 <saifulislamx264@gmail.com>
2023-11-19 13:34:22 +0600

    server setup fully done

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import config from "./app/index";
-import app from "./app";
+import mongoose from 'mongoose';
+import config from './app/index';
+import app from './app';
 
 // main().catch(err => console.log(err));
 
@@ -9,7 +9,7 @@ async function main() {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`App listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
